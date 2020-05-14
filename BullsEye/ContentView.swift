2cf22dcec975b771ad969b0ad6191b1ab9d3343e
Viveka Anban <viveka98@hotmail.ca>
2020-05-14 14:48:45 -0400

    Added all the views in the game page

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -11,28 +11,70 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app")
-                .fontWeight(.bold)
-                .foregroundColor(Color.green)
-            Button(action: {
-                print("Button Pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-            }
-            .alert(isPresented: $alertIsVisible) { () ->
-                Alert in
-                return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+            VStack {
+                
+                Spacer()
+                
+                //Target row
+                HStack {
+                    Text("Put the bullseye as cloase as you can to: ")
+                    Text("100")
+                }
+                
+                Spacer()
+                
+                //Slider row
+                HStack{
+                    Text("1")
+                    Slider(value: self.$sliderValue, in: 1...100)
+                    Text("100")
+                }
+                
+                Spacer()
+
+                //Hit Me! Button row
+                Button(action: {
+                    print("Button Pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Hello There!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                }
+                
+                Spacer()
+                
+                //Lower Buttons row
+                HStack{
+                    Button(action: {}){
+                        Text("Start Over")
+                    }
+                    Spacer()
+                    Text("Score: ")
+                    Text("999999")
+                    Spacer()
+                    Text("Round: ")
+                    Text("999")
+                    Spacer()
+                    Button(action: {}){
+                        Text("Info")
+                    }
+                    
+                }
+                .padding(.bottom, 20)
             }
         }
     }
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
-            ContentView()
+            ContentView().previewLayout(.fixed(width: 896, height: 414))
         }
     }
 }
